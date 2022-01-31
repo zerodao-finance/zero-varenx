bc0e04eaf78f3e7e589b79f2d290a4c556b17ddb
altinak <altinak@zero.gg>
2022-01-31 12:52:33 -0600

    VarenSwap.sol

---
+++ hardhat.config.js
@@ -0,0 +1,10 @@
+require('@nomiclabs/hardhat-ethers');
+require('hardhat-deploy');
+require('hardhat-deploy-ethers');
+require('hardhat-gas-reporter');
+require('@openzeppelin/hardhat-upgrades');
+require('@nomiclabs/hardhat-etherscan');
+
+module.exports = {
+  solidity: '0.8.6'
+};
